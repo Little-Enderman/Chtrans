973e8a0b904368ba1d54ef914de2a7d1cf09f66a
Little-Enderman <60375306+Little-Enderman@users.noreply.github.com>
2020-03-28 14:22:06 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'chtran',
+    version = '0.0.1',
+    keywords='Ch zh',
+    description = 'a library for Translate to Chinese',
+    license = 'MIT License',
+    url = 'https://github.com/Little-Enderman/Chtrans',
+    author = 'Luca Liu',
+    author_email = 'Endermanwithhim@icloud.com',
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = 'any',
+    install_requires = [],
+)
